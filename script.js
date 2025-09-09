4f6307854bd6f0b5bf1e7e1bf822dd8207864b9b
cecilia8I <emeyng07@gmail.com>
2025-09-08 22:48:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function sayYes() {
   alert("Yayy! 🥳🎉 I’m so excited!! ❤️");
   alert("Let’s do something fun together 💘");
-  alert("Will you drive though? 🤔");
+  alert("Will you drive though? 🤔(Close = Yes)");
   alert("Yayyyyyyyyyy!");
 }
 
 function sayNo() {
   alert("I'm sorry this is not an option");
   alert("Choose again");
-  alert(`Press "OK" if you want to go on a sunset date with me`)
+  alert(`Press "Close" if you want to go on a sunset date with me`)
   alert("Okay COOL!")
 }
